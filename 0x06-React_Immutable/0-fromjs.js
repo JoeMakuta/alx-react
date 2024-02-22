547311b8904a69c160f7f60652a15b81d02aee9b
JoeMakuta <makutajosue@gmail.com>
2024-02-22 14:02:06 +0200

    Fix node module

--- 0x06-React_Immutable/0-fromjs.js
+++ 0x06-React_Immutable/0-fromjs.js
@@ -1,5 +1,14 @@
-import { fromJS } from "immutable";
+const immutable = require("immutable");
 
-const getImmutableObject = (object) => fromJS(object);
+const getImmutableObject = (object) => immutable.fromJS(object);
 
-export default getImmutableObject;
+console.log(
+  getImmutableObject({
+    fear: true,
+    smell: -1033575916.9145899,
+    wall: false,
+    thing: -914767132,
+  })
+);
+
+module.exports = getImmutableObject;
